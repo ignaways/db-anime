0525682b847bb336893bdaba50bab9c48df7d99e
ignaway <ignaway@gmail.com>
2022-12-12 12:56:42 +0700

    update payload

--- models/AnimeModels.js
+++ models/AnimeModels.js
@@ -4,7 +4,7 @@ const { HEADERS, PATH_ANIME } = require('../constant/path')
 class AnimeModels {
   static async getList(year, season) {
     try {
-      const response = await axios.get(`${PATH_ANIME}/season/${year}/${season}?limit=8&sort=anime_num_list_users&fields=studios,num_episodes,broadcast,source,mean,synopsis,genres,media_type`, HEADERS)
+      const response = await axios.get(`${PATH_ANIME}/season/${year}/${season}?limit=6&sort=anime_num_list_users&fields=studios,num_episodes,broadcast,source,mean,synopsis,genres,media_type,start_date,alternative_titles`, HEADERS)
       return response.data
     } catch (error) {
       console.log(error, '<<<error')
